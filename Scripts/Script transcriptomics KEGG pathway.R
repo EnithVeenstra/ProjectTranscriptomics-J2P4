fc1ab720dac8a9e357ee22e0f0b88d7f183c7741
EnithVeenstra <enith.veenstra@student.nhlstenden.com>
2025-06-14 23:26:50 +0200

    UPDATE

---
+++ Scripts/Script transcriptomics KEGG pathway.R
@@ -0,0 +1,15 @@
+# setwd -> Transcriptomics
+getwd()
+
+gene_vector <- resultatenproject$log2FoldChange
+names(gene_vector) <- rownames(resultatenproject)
+
+
+# KEGG map00061 = Fatty acid biosynthesis → pathway ID = "04110"
+pathview(gene.data = gene_vector,
+         pathway.id = "hsa04110",
+         species = "hsa",  
+         gene.idtype = "SYMBOL", # hsa = Homo sapiens
+         kegg.native = TRUE)
+
+hsa04110.cellcycle.pathview.png
